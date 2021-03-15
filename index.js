3a3c4b0c9170a5397d2954f9c01e2b847b478a7f
Vitor Quadros <viitorquadros@hotmail.com>
2021-03-15 14:38:22 -0300

    refactoring structure

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var bodyParser = require('body-parser')
-var express = require("express")
-var app = express()
-var router = require("./routes/routes")
- 
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const router = require("./routes/routes");
+
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use("/",router);
 
-app.listen(8686,() => {
-    console.log("Servidor rodando")
+app.listen(3333,() => {
+    console.log("Running...");
 });
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,11 @@
-var knex = require('knex')({
-    client: 'mysql2',
+var knex = require("knex")({
+    client: "mysql2",
     connection: {
-      host : '127.0.0.1',
-      user : 'root',
-      password : '',
-      database : 'usertest'
+        host : "127.0.0.1",
+        user : "root",
+        password : "8642",
+        database : "apiusers"
     }
-  });
+});
 
-module.exports = knex
+module.exports = knex;
