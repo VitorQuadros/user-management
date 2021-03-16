fdd5e607a7192f6b519efda7a2af42dfc8564095
Vitor Quadros <viitorquadros@hotmail.com>
2021-03-16 15:22:14 -0300

    feat (user model): find by email method

--- src/models/User.js
+++ src/models/User.js
@@ -27,6 +27,20 @@ class User {
         }
     }
 
+    async findByEmail(email) {
+        try {
+            const result = await knex.select(["id", "name", "email", "role"]).where({email}).table("users");
+            if (result.length > 0) {
+                return result[0];
+            } else {
+                return undefined;
+            }
+        } catch (error) {
+            console.log(error);
+            return undefined;
+        }
+    }
+
 
     async new(email, password, name){
         try {
